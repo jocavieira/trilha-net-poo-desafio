e3181a65d22b74f605eeaa1d69feda9a95a914aa
Joca Vieira <jocavieira@Jocas-MacBook-Air.local>
2025-10-23 15:28:13 -0300

    add desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 32);
+Iphone iphone = new Iphone("987654321", "iPhone 13", "987654321", 128);
+
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
