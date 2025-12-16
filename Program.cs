50d685555c260c1ad845f1888fd4b060b3189c7c
Joca Vieira <jocavieira@Jocas-MacBook-Air.local>
2025-12-16 11:23:20 -0300

    fix conflicts

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // Creating phone instances
-Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 32);
-Iphone iphone = new Iphone("987654321", "iPhone 13", "987654321", 128);
+Nokia nokia = new Nokia("123456789", "Nokia 3310", 32);
+Iphone iphone = new Iphone("987654321", "iPhone 13", 128);
 
 // Nokia actions
 nokia.Call();           
